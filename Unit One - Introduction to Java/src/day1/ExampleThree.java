df52d071f60b8b5429d7ac95ec0452cd3bc84bbc
Daisy Mou <dmou@bayviewglen.ca>
2024-09-16 08:46:35 -0400

    Day 1 Examples 3&4

---
+++ Unit One - Introduction to Java/src/day1/ExampleThree.java
@@ -0,0 +1,38 @@
+package day1;
+
+public class ExampleThree {
+
+    public static void main(String[] args) {
+        int numStudents = 14; // variables use camelCase
+        double volume = 123.7;
+        boolean largeClass = numStudents > 20;
+
+        System.out.println(numStudents);
+        System.out.println(volume);
+        System.out.println(largeClass);
+
+        int a = 5, b = 7;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+
+        double c = 6.5, d = 10;
+        System.out.println(d);
+        System.out.println(c + d);
+
+        System.out.println(c / d);
+        System.out.println(a / b);
+        System.out.println(b / a);
+
+        System.out.println(7 % 5); // 7/5 has a remainder of 2
+                                   // % is the remainder
+        System.out.println();
+
+        final double h = 2.3;
+        double j = 2.6;
+        double i;
+
+    }
+
+}
---
+++ Unit One - Introduction to Java/src/day1/ExampleTwo.java
@@ -0,0 +1,18 @@
+package day1; //semi-colon is the end of a statement
+
+public class ExampleTwo {
+    // are used to indicate something
+    // {} is used to embed something inside
+
+    public static void main(String[] args) {
+
+        System.out.println("This is harder than python"); // prints first then indents(enter)
+        System.out.println("Sysout makes it faster"); // use sysout as a shortcut
+        System.out.println();
+
+        System.out.println("A");
+        System.out.print("hello"); // Does not add a new line (no space or indent)
+
+    }
+
+}
